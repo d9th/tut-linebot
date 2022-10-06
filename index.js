ff62d9dd419826afb8e00b00c647a14aa72464b4
Shunsuke Shiina <gravitate.karma@gmail.com>
2022-10-06 20:18:01 +0900

    using linebot-sdk

--- index.js
+++ index.js
@@ -1,78 +1,38 @@
-const https = require("https");
+const line = require("@line/bot-sdk");
 const express = require("express");
-const crypto = require("crypto");
+
+const config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const TOKEN = process.env.LINE_ACCESS_TOKEN;
-const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
-
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.sendStatus(200);
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-app.post("/webhook", (req, res) => {
-  res.send("http post request sent to the webhook url.");
-  console.log(req.body);
-  // const dataBody = JSON.parse(req.body);
-  console.dir(req.body.events);
-  // TODO
-  const signature = crypto
-    .createHmac("SHA256", CHANNEL_SECRET)
-    .update(JSON.stringify(req.body))
-    .digest("base64");
-
-  console.log(signature === req.get("x-line-signature"));
-
-  if (
-    req.get("x-line-signature") === signature &&
-    req.body.events[0].type === "message"
-  ) {
-    const dataString = JSON.stringify({
-      replyToken: req.body.events[0].replyToken,
-      messages: [
-        {
-          type: "text",
-          text: `You said ${req.body.events[0].message.text}`,
-        },
-      ],
-    });
-
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    };
-
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    const request = https.request(webhookOptions, res => {
-      res.on("data", d => {
-        process.stdout.write(d);
-      });
-    });
-
-    request.on("error", err => {
-      console.error(err.message);
-    });
-
-    request.write(dataString);
-    request.end();
+const handleEvent = event => {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
   }
-});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  const echo = {
+    type: "text",
+    text: event.message.text,
+  };
+
+  return client.replyMessage(event.replyToken, echo);
+};
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on ${post}`);
 });
